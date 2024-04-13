63c3141792b6a1f3c660f2e0e15d9b7f5db7b8e0
Adil <adil.hussain1601@gmail.com>
2024-04-14 02:44:26 +0530

    folder added

---
+++ experiment/experiment.js
@@ -0,0 +1,18 @@
+const timesToRepeat=10;
+const char="💝";
+let ans="";
+for(i=0;i<timesToRepeat;i++){
+    ans+=char;
+} 
+console.log(ans);
+
+const number = 5.3;
+const roundedNumber = number.toPrecision(1);
+console.log(roundedNumber);
+
+const mystring="adil is my name";
+const findString="adil"
+console.log(mystring.includes(findString))
+
+console.log(Date())
+
